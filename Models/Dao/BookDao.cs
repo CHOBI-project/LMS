385ac46e1f26b5bd730469ada48e5b9c6514edf9
CHOBI <139092470+CHOBI-project@users.noreply.github.com>
2025-09-04 22:34:42 +0900

    :sparkles: feature find all books.

--- Models/Dao/BookDao.cs
+++ Models/Dao/BookDao.cs
@@ -9,14 +9,36 @@ public class BookDao : BaseDao<BookEntity>, IBookDao
 {
     public BookDao(IConfiguration configuration, ILogger<BookEntity> logger) : base(configuration, logger) {}
 
+    public List<BookEntity> FindAll()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        connection.Open();
+
+        string sql = "SELECT * FROM Books";
+        using var cmd = new MySqlCommand(sql, connection);
+        using var result = cmd.ExecuteReader();
+
+        List<BookEntity> books = new List<BookEntity>();
+        while (result.Read()) 
+        {
+            books.Add(new BookEntity()
+            {
+                Isbn = result.GetString("Isbn"),
+                Title = result.GetString("Title"),
+                Author = result.GetString("Author"),
+            });
+        }
+        return books;
+    }
+
     public BookEntity FindByIsbn(BookEntity entity)
     {
         using var connection = new MySqlConnection(ConnectionString);
         connection.Open();
 
-        string sql = "SELECT * FROM Books WHERE Title = @title";
+        string sql = "SELECT * FROM Books WHERE Isbn = @isbn";
         using var cmd = new MySqlCommand(sql, connection);
-        cmd.Parameters.Add("@title", MySqlDbType.VarChar, 13).Value = entity.Title;
+        cmd.Parameters.Add("@isbn", MySqlDbType.VarChar, 13).Value = entity.Isbn;
 
         using var result = cmd.ExecuteReader();
 
@@ -38,9 +60,9 @@ public class BookDao : BaseDao<BookEntity>, IBookDao
         using var connection = new MySqlConnection(ConnectionString);
         connection.Open();
 
-        string sql = "SELECT * FROM Books WHERE Isbn = @isbn";
+        string sql = "SELECT * FROM Books WHERE Title = @title";
         using var cmd = new MySqlCommand(sql, connection);
-        cmd.Parameters.Add("@isbn", MySqlDbType.VarChar, 255).Value = entity.Isbn;
+        cmd.Parameters.Add("@title", MySqlDbType.VarChar, 255).Value = entity.Title;
 
         using var result = cmd.ExecuteReader();
 
